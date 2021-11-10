c229c6d4d13a8742999589f56a22e19dc3ad4fbc
DaegyeomHa <combbm@naver.com>
2021-11-10 23:06:56 +0900

    fix: delete unnecessary files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es6: true
+  },
   extends: ['airbnb', 'airbnb/hooks', 'eslint-config-prettier'],
   plugins: ['react', '@typescript-eslint'],
   rules: {
