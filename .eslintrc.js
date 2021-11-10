3ec860b2718bca6cdb3642be9e5c980ca9caa289
DaegyeomHa <combbm@naver.com>
2021-11-10 23:04:26 +0900

    chore: use yarn v1

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'eslint-config-prettier'],
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off'
+  }
+};
