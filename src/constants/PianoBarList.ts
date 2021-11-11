227a83c1ce7d16725fd086374db90869ceec2f8a
DaegyeomHa <combbm@naver.com>
2021-11-11 17:50:41 +0900

    feat: 88키 피아노

--- src/constants/PianoBarList.ts
+++ src/constants/PianoBarList.ts
@@ -8,6 +8,166 @@ interface ListType {
 export const pianobarList: ListType[] = [
   {
     color: 'white',
+    note: 'A0'
+  },
+  {
+    color: 'black',
+    note: 'A#0'
+  },
+  {
+    color: 'white',
+    note: 'B0'
+  },
+
+  {
+    color: 'white',
+    note: 'C1'
+  },
+  {
+    color: 'black',
+    note: 'C#1'
+  },
+  {
+    color: 'white',
+    note: 'D1'
+  },
+  {
+    color: 'black',
+    note: 'D#1'
+  },
+  {
+    color: 'white',
+    note: 'E1'
+  },
+  {
+    color: 'white',
+    note: 'F1'
+  },
+  {
+    color: 'black',
+    note: 'F#1'
+  },
+  {
+    color: 'white',
+    note: 'G1'
+  },
+  {
+    color: 'black',
+    note: 'G#1'
+  },
+  {
+    color: 'white',
+    note: 'A1'
+  },
+  {
+    color: 'black',
+    note: 'A#1'
+  },
+  {
+    color: 'white',
+    note: 'B1'
+  },
+
+  {
+    color: 'white',
+    note: 'C2'
+  },
+  {
+    color: 'black',
+    note: 'C#2'
+  },
+  {
+    color: 'white',
+    note: 'D2'
+  },
+  {
+    color: 'black',
+    note: 'D#2'
+  },
+  {
+    color: 'white',
+    note: 'E2'
+  },
+  {
+    color: 'white',
+    note: 'F2'
+  },
+  {
+    color: 'black',
+    note: 'F#2'
+  },
+  {
+    color: 'white',
+    note: 'G2'
+  },
+  {
+    color: 'black',
+    note: 'G#2'
+  },
+  {
+    color: 'white',
+    note: 'A2'
+  },
+  {
+    color: 'black',
+    note: 'A#2'
+  },
+  {
+    color: 'white',
+    note: 'B2'
+  },
+
+  {
+    color: 'white',
+    note: 'C3'
+  },
+  {
+    color: 'black',
+    note: 'C#3'
+  },
+  {
+    color: 'white',
+    note: 'D3'
+  },
+  {
+    color: 'black',
+    note: 'D#3'
+  },
+  {
+    color: 'white',
+    note: 'E3'
+  },
+  {
+    color: 'white',
+    note: 'F3'
+  },
+  {
+    color: 'black',
+    note: 'F#3'
+  },
+  {
+    color: 'white',
+    note: 'G3'
+  },
+  {
+    color: 'black',
+    note: 'G#3'
+  },
+  {
+    color: 'white',
+    note: 'A3'
+  },
+  {
+    color: 'black',
+    note: 'A#3'
+  },
+  {
+    color: 'white',
+    note: 'B3'
+  },
+
+  {
+    color: 'white',
     note: 'C4'
   },
   {
@@ -54,8 +214,156 @@ export const pianobarList: ListType[] = [
     color: 'white',
     note: 'B4'
   },
+
   {
     color: 'white',
     note: 'C5'
+  },
+  {
+    color: 'black',
+    note: 'C#5'
+  },
+  {
+    color: 'white',
+    note: 'D5'
+  },
+  {
+    color: 'black',
+    note: 'D#5'
+  },
+  {
+    color: 'white',
+    note: 'E5'
+  },
+  {
+    color: 'white',
+    note: 'F5'
+  },
+  {
+    color: 'black',
+    note: 'F#5'
+  },
+  {
+    color: 'white',
+    note: 'G5'
+  },
+  {
+    color: 'black',
+    note: 'G#5'
+  },
+  {
+    color: 'white',
+    note: 'A5'
+  },
+  {
+    color: 'black',
+    note: 'A#5'
+  },
+  {
+    color: 'white',
+    note: 'B5'
+  },
+
+  {
+    color: 'white',
+    note: 'C6'
+  },
+  {
+    color: 'black',
+    note: 'C#6'
+  },
+  {
+    color: 'white',
+    note: 'D6'
+  },
+  {
+    color: 'black',
+    note: 'D#6'
+  },
+  {
+    color: 'white',
+    note: 'E6'
+  },
+  {
+    color: 'white',
+    note: 'F6'
+  },
+  {
+    color: 'black',
+    note: 'F#6'
+  },
+  {
+    color: 'white',
+    note: 'G6'
+  },
+  {
+    color: 'black',
+    note: 'G#6'
+  },
+  {
+    color: 'white',
+    note: 'A6'
+  },
+  {
+    color: 'black',
+    note: 'A#6'
+  },
+  {
+    color: 'white',
+    note: 'B6'
+  },
+
+  {
+    color: 'white',
+    note: 'C7'
+  },
+  {
+    color: 'black',
+    note: 'C#7'
+  },
+  {
+    color: 'white',
+    note: 'D7'
+  },
+  {
+    color: 'black',
+    note: 'D#7'
+  },
+  {
+    color: 'white',
+    note: 'E7'
+  },
+  {
+    color: 'white',
+    note: 'F7'
+  },
+  {
+    color: 'black',
+    note: 'F#7'
+  },
+  {
+    color: 'white',
+    note: 'G7'
+  },
+  {
+    color: 'black',
+    note: 'G#7'
+  },
+  {
+    color: 'white',
+    note: 'A7'
+  },
+  {
+    color: 'black',
+    note: 'A#7'
+  },
+  {
+    color: 'white',
+    note: 'B7'
+  },
+
+  {
+    color: 'white',
+    note: 'C8'
   }
 ];
