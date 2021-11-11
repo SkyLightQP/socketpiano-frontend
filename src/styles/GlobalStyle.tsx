edbf9e8b0a99e57cafe7c7d967ee69429ccec5a5
DaegyeomHa <combbm@naver.com>
2021-11-11 20:57:16 +0900

    feat: Pretendard 적용

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -5,10 +5,17 @@ const GlobalStyle: React.FC = () => {
   return (
     <Global
       styles={css`
+        @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css');
+
         * {
           margin: 0;
           padding: 0;
           box-sizing: border-box;
+        }
+
+        body {
+          font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI',
+            'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
         }
 
         a {
