cfb0b63de2cbaa12b12b2e3968be536ce83787b2
DaegyeomHa <combbm@naver.com>
2021-11-13 21:21:14 +0900

    feat: add keyboard event into piano

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { KeyboardEvent, useMemo } from 'react';
 import styled from '@emotion/styled';
 import * as Tone from 'tone';
 import { Note } from 'tone/build/esm/core/type/NoteUnits';
@@ -15,6 +15,10 @@ const Container = styled.div`
 
   width: 100vw;
   height: 100vh;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const PianoContainer = styled.div`
@@ -44,8 +48,48 @@ const MainPage: React.FC = () => {
     piano.volume.value = vol;
   };
 
+  const onKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
+    switch (e.key.toLowerCase()) {
+      case 'a':
+        onBarClick('C4');
+        break;
+      case 's':
+        onBarClick('D4');
+        break;
+      case 'd':
+        onBarClick('E4');
+        break;
+      case 'f':
+        onBarClick('F4');
+        break;
+      case 'g':
+        onBarClick('G4');
+        break;
+      case 'h':
+        onBarClick('A4');
+        break;
+      case 'j':
+        onBarClick('B4');
+        break;
+      case 'k':
+        onBarClick('C5');
+        break;
+      case 'l':
+        onBarClick('D5');
+        break;
+      case ';':
+        onBarClick('E5');
+        break;
+      case "'":
+        onBarClick('F5');
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <Container>
+    <Container tabIndex={0} onKeyDown={onKeyPress}>
       <PianoContainer>
         <Piano onBarClick={onBarClick} />
       </PianoContainer>
